72236be7d23c1cf377ca467cacb843678c1554dd
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-04-13 05:35:32 +0100

    adjust for lighthouse

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   //   console.log("got here");
   //   res.send("overhere");
+  res.status = 200;
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-app.listen(PORT, () => {
-  console.log(`you are currently listening on port${PORT}`);
+app.use(function (req, res) {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not Found");
 });
+app.use((err, req, res, next) => {
+  if (err) {
+    res.send(err.stack);
+  }
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 - Server Error");
+});
+app.listen(PORT);

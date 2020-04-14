e2153b8e4d89064965b52d28dac0d7c3d3048de4
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-04-14 05:08:20 +0100

    padded time with 0

--- Routes/index.js
+++ Routes/index.js
@@ -19,7 +19,9 @@ const jsonResponse = (req, res) => {
   const result = covidEstimator(req.body);
   res.status(200).json(result);
   const time = new Date() - start;
-  const text = `${req.method}   ${req.baseUrl}${req.url}    ${res.statusCode}   ${time}ms`;
+  const text = `${req.method}   ${req.baseUrl}${req.url}    ${
+    res.statusCode
+  }   ${0 + time}ms`;
   readfile(text);
 };
 const xmlResponse = (req, res) => {
@@ -29,7 +31,9 @@ const xmlResponse = (req, res) => {
   const xmlformat = js2xmlparser.parse("body", result);
   res.status(200).send(xmlformat);
   const time = new Date() - start;
-  const text = `${req.method}   ${req.baseUrl}${req.url}   ${res.statusCode}    ${time}ms`;
+  const text = `${req.method}   ${req.baseUrl}${req.url}   ${
+    res.statusCode
+  }    ${0 + time}ms`;
   readfile(text);
 };
 const logResponse = (req, res) => {
